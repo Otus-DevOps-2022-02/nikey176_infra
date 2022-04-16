dc4b900f6e965859a868978beab7ea96f46eaafd
Nikolai Chernukha <chernukha84@yandex.ru>
2022-04-16 18:22:39 +0300

    fix: terraform-2 Фикс для автотестов #4

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.73.0"
-    }
-  }
-}
-
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   folder_id                = var.folder_id
